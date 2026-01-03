fc244a85122f6c6ed05df1d313ac1595c1291b7a
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-01-03 13:22:02 +1000

    updated animation design sytem and created helper file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  devToolbar: {
+    enabled: false,
+  },
 });
---
+++ src/lib/helper.ts
@@ -0,0 +1,9 @@
+export function cssTime(varName: string) {
+  const value = getComputedStyle(document.documentElement)
+    .getPropertyValue(varName)
+    .trim();
+
+  if (value.endsWith("ms")) return parseFloat(value) / 1000;
+  if (value.endsWith("s")) return parseFloat(value);
+  return parseFloat(value);
+}
