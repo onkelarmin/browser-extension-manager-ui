0e7f31412b070af64bde20a66034189c75c22459
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-01-06 17:50:45 +1000

    finished project

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,4 @@
+// Convert time value from CSS to JS
 export function cssTime(varName: string) {
   const value = getComputedStyle(document.documentElement)
     .getPropertyValue(varName)
@@ -6,4 +7,13 @@ export function cssTime(varName: string) {
   if (value.endsWith("ms")) return parseFloat(value) / 1000;
   if (value.endsWith("s")) return parseFloat(value);
   return parseFloat(value);
+}
+
+// Get CSS custom properties in JS
+export function customProp(varName: string) {
+  const value = getComputedStyle(document.documentElement)
+    .getPropertyValue(varName)
+    .trim();
+
+  return value;
 }
